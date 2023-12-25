fc9f1c4a93ae98ecc1a4b5ebe477ecf8cf589c2a
AhmedEliraqi <127881284+AhmedEliraqi@users.noreply.github.com>
2023-12-25 15:53:02 +0200

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiUrl:"https://localhost:44348/api/"
+    //apiUrl:"https://localhost:44348/api/"
+   apiUrl:"http://ahmedibrahim66-001-site1.btempurl.com/api/"
 };
